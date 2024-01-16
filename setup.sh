99751bf555411cdc8fdc122fb106b8a252a34458
Luiz Ozorio <luiz.ozorio@vectorvest.com>
2024-01-15 20:27:54 -0500

    refactor: set TOS last

---
+++ setup.sh
@@ -0,0 +1,24 @@
+# setup arducam
+wget -O install_pivariety_pkgs.sh https://github.com/ArduCAM/Arducam-Pivariety-V4L2-Driver/releases/download/install_script/install_pivariety_pkgs.sh
+chmod +x install_pivariety_pkgs.sh
+
+# libcamera
+./install_pivariety_pkgs.sh -p libcamera_dev
+./install_pivariety_pkgs.sh -p libcamera_apps
+
+# Hawkeye drivers
+./install_pivariety_pkgs.sh -p 64mp_pi_hawk_eye_kernel_driver
+
+# Get git
+sudo apt install git
+
+# Clone rpanion
+cd ~/ && git clone --recursive https://github.com/stephendade/Rpanion-server.git
+cd ./Rpanion-server/deploy && ./RasPi2-3-4-5-deploy.sh && ./deploy/wifi_access_point.sh
+
+# Prevent ssh freezing by setting TOS (type Of Service) field to Cs0, Cs0 (aka 0x00, 0x00) <==> (best effort, best effort)
+echo "IPQoS cs0 cs0" >> /etc/ssh/sshd_config
+
+# Reboot
+sudo reboot
+
