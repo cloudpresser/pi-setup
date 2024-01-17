64f90fb629990a33f9ffe4843735bc1adeca09a9
Luiz Ozorio <luiz.ozorio@vectorvest.com>
2024-01-17 15:37:52 -0500

    feat: separate commands

--- setup.sh
+++ setup.sh
@@ -1,9 +1,16 @@
 # Get git & tmux
-sudo apt install git
+sudo apt install git tmux
 
 # Prevent ssh freezing by setting TOS (type Of Service) field to Cs0, Cs0 (aka 0x00, 0x00) <==> (best effort, best effort)
 echo "IPQoS cs0 cs0" >> /etc/ssh/sshd_config
 
+cd ~/
+
 # Clone rpanion
-cd ~/ && git clone --recursive https://github.com/stephendade/Rpanion-server.git
+git clone --recursive https://github.com/stephendade/Rpanion-server.git
+
+# Clone pi-setup
+git clone https://github.com/cloudpresser/pi-setup.git
+
+# Deploy rpanion
 cd ./Rpanion-server/deploy && tmux new-session -s deploy_rpanion './RasPi2-3-4-5-deploy.sh'
